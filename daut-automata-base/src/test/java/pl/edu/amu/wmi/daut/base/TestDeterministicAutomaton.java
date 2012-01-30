ac0ad6e011ea11144874a54110f9ff456c1104b0
Ola Maj <glajda84@tlen.pl>
2012-01-30 15:21:39 +0100

    up do date

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestDeterministicAutomaton.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestDeterministicAutomaton.java
@@ -17,29 +17,28 @@ public class TestDeterministicAutomaton extends TestCase {
         	State q0 = test.addState();
         	State q1 = test.addState();
         	State q2 = test.addState();
-                /*State q3 = test.addState();*/
+                State q3 = test.addState();
         
         
         	test.addLoop(q0, new CharTransitionLabel('1'));
         	test.addTransition(q0, q1, new CharTransitionLabel('0'));
         	test.addTransition(q1, q2, new CharTransitionLabel('0'));
         	test.addTransition(q2, q0, new CharTransitionLabel('0'));
-                /*test.addTransition(q3, q1, new CharTransitionLabel('0'));*/
+                test.addTransition(q3, q1, new CharTransitionLabel('0'));
         	test.addLoop(q1, new CharTransitionLabel('1'));
         	test.addLoop(q2, new CharTransitionLabel('1'));
-                /*test.addLoop(q3, new CharTransitionLabel('1'));*/
+                test.addLoop(q3, new CharTransitionLabel('1'));
                 
 			
         
         	test.markAsInitial(q0);
-        	test.markAsFinal(q0);
+        	test.markAsFinal(q3);
                 
 /**
  * at -> automaton test
  */
 
 		final AutomatonByRecursion at = new AutomatonByRecursion(test);
-                assertTrue(at.accepts("111"));
                 assertTrue(at.accepts("000"));
         	assertTrue(at.accepts("1000"));
         	assertTrue(at.accepts("0001"));
