d058e400c5feccba47802e56284af61597556bd9
Ola Maj <glajda84@tlen.pl>
2012-02-04 01:06:21 +0100

    dodanie pliku glajda84_demo.sh

---
+++ poligon/glajda84_demo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+perl glajda84_wybor_godzin.pl glajda84_test | sort -k 1rn
+
+# ustalenie frekwencji i godzin dla znalezionych godzin (wybor_godzin.pl)
+# sortowanie godzin wg frekwencji
